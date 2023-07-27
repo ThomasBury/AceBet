eab6f38149ff5a549dd513ab65c3c4f72c545d5c
Thomas Bury <bury.thomas@gmail.com>
2023-07-27 20:06:17 +0200

    fix: remove imports, fix paths and file extensions and add main

--- src/models/train.py
+++ src/models/train.py
@@ -1,22 +1,11 @@
 import pandas as pd
+from pathlib import Path
 from lightgbm import LGBMClassifier
 from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
 from sklearn.model_selection import TimeSeriesSplit
-from sklearn.metrics import classification_report, accuracy_score, roc_auc_score
 from joblib import dump
-from datetime import datetime, timedelta
-from dateutil.relativedelta import relativedelta
-
+from datetime import datetime
 from arfs.preprocessing import OrdinalEncoderPandas
-from arfs.feature_selection import (
-    Leshy,
-    MissingValueThreshold,
-    UniqueValuesThreshold,
-    CollinearityThreshold,
-    VariableImportance,
-    make_fs_summary
-)
 
 def prepare_data_for_training_clf(start_date, end_date):
     """
@@ -35,15 +24,15 @@ def prepare_data_for_training_clf(start_date, end_date):
         The prepared data.
 
     """
-
-    df = pd.read_feather("../data/atp_data_production.csv")
-    df['Date'] = pd.to_datetime(df['Date'])
+    data_path = Path(__file__).resolve().parents[2] / 'data' / 'atp_data_production.feather'
+    df = pd.read_feather(data_path)
+    df['date'] = pd.to_datetime(df['date'])
     df = df.query('date >= @start_date and date <= @end_date')
 
     # Create predictors list
-    predictors = df.columns.drop(['target', 'date', 'sets_p1', 'sets_p2', "p1_won_more_sets", 'b365_p1', 'b365_p2', 'ps_p1', 'ps_p2'])
+    predictors = df.columns.drop(['target', 'date', 'sets_p1', 'sets_p2', 'b365_p1', 'b365_p2', 'ps_p1', 'ps_p2'])
     X = df[predictors].copy()
-    y = df['target'].copy() * 1
+    y = df['target'].values.copy() * 1
 
     return X, y
 
@@ -75,7 +64,7 @@ def time_series_split(X, y, n_splits=2):
     train_idx, test_idx = all_splits[0]
     return train_idx, test_idx
 
-def train_model(X_train, y_train, start_date, end_date):
+def train_model(start_date, end_date):
     """
     Train a model on the training data.
 
@@ -102,7 +91,11 @@ def train_model(X_train, y_train, start_date, end_date):
 
     model.fit(X_train, y_train)
     today = datetime.today()
+    model_path = Path(__file__).resolve().parents[2] / "trained_models" / "model_{today.strftime('%Y-%m-%d-%H-%M')}.joblib"
     filename = f"./model_{today.strftime('%Y-%m-%d-%H-%M')}.joblib"
     dump(model, filename)
 
-
+if __name__ == "__main__":
+    start_date = "2016-03-04"
+    end_date = "2018-03-04"
+    train_model(start_date, end_date)
